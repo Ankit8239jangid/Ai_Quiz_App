2f24f9e63c8f2e33ddd48a94f26053e23d171bdb
ankit <ankitjangid6375@gmail.com>
2025-04-08 11:22:27 -0400

    adding cors for production

--- backend/index.js
+++ backend/index.js
@@ -10,14 +10,15 @@ const app = express();
 app.use(express.json());
 app.use(
     cors({
-      origin: '' // replace with your actual frontend URL
+        origin: 'https://ai-quiz-by-ankit.vercel.app',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
     })
-  );
+);
 
 app.use('/api/v1/', router);
 
 app.get("/", (req, res) => {
-    res.send("hyy i an ankit ");
+    res.send("hyy i an ankit and this is a Ai-quiz App Api ");
 
 });
 
